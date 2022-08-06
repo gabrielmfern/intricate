32e699d14371973fd69cfe7cd803c02882ff44b4
Gabriel Miranda <gabrielmfern@outlook.com>
2022-08-06 12:41:57 -0300

    fix a bug with the backprop in the TanHGPU layer and write a unit test for it

--- src/layers/activations/kernels/tanh.cl
+++ src/layers/activations/kernels/tanh.cl
@@ -12,12 +12,28 @@ kernel void back_propagate(
     global float* flattened_loss_to_output_derivatives,
     global float* flattened_output_samples,
 
-    global float* flattened_loss_to_input_derivatives
+    global float* flattened_loss_to_input_derivatives,
+
+    int outputs_amount
 ) {
-    int index = get_global_id(0);
+    int sample_index = get_global_id(0);
+    int samples_amount = get_global_size(0);
 
-    float val = flattened_output_samples[index];
-    float output_to_input_derivative = 1 - val * val;
+    int input_index = get_global_id(1);
+    int inputs_amount = get_global_size(1);
 
-    flattened_loss_to_input_derivatives[index] = output_to_input_derivative * flattened_loss_to_output_derivatives[index];
+    int flat_input_i = sample_index * inputs_amount + input_index;
+
+    float total = 0.0;
+
+    for (int output_index = 0; output_index < outputs_amount; output_index++) {
+        int flat_output_i = sample_index * outputs_amount + output_index;
+
+        float output = flattened_output_samples[flat_output_i];
+        float output_to_input_derivative = 1 - output * output;
+
+        total += output_to_input_derivative * flattened_loss_to_output_derivatives[flat_output_i];
+    }
+
+    flattened_loss_to_input_derivatives[flat_input_i] = total;
 }
--- src/layers/activations/tanh_gpu.rs
+++ src/layers/activations/tanh_gpu.rs
@@ -4,8 +4,6 @@ use crate::layers::OpenCLLayer;
 #[allow(unused_imports)]
 use crate::utils::approx_eq::assert_approx_equal_distance;
 
-use std::mem;
-use std::ptr;
 use opencl3::memory::ClMem;
 #[allow(unused_imports)]
 use opencl3::{
@@ -17,6 +15,8 @@ use opencl3::{
     memory::{Buffer, CL_MEM_READ_ONLY, CL_MEM_READ_WRITE},
     program::Program,
 };
+use std::mem;
+use std::ptr;
 
 use savefile_derive::Savefile;
 
@@ -36,45 +36,154 @@ fn should_return_same_value_as_normal_tanh_function() -> Result<(), ClError> {
     let queue = CommandQueue::create_with_properties(&context, device_ids[0], 0, 0)?;
 
     let mut normal_tanh = TanH::new();
-    let mut gpu_tanh = TanHGPU::new(&context, &queue)?;
-    
+    let mut gpu_tanh = TanHGPU::new(100, &context, &queue)?;
+
     let input_samples = vec![vec![0.412; 100]; 100];
     let expected_outputs = normal_tanh.propagate(&input_samples);
 
-    let mut input_samples_buffer = Buffer::<cl_float>::create(
-        &context,
-        CL_MEM_READ_ONLY,
-        100 * 100,
-        ptr::null_mut()
-    )?;
+    let mut input_samples_buffer =
+        Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, 100 * 100, ptr::null_mut())?;
 
-    queue.enqueue_write_buffer(
-        &mut input_samples_buffer, 
-        CL_BLOCKING, 
-        0, 
-        input_samples.iter().map(|v| v.to_vec()).flatten().collect::<Vec<f32>>().as_slice(),
-        &[]
-    )?.wait()?;
+    queue
+        .enqueue_write_buffer(
+            &mut input_samples_buffer,
+            CL_BLOCKING,
+            0,
+            input_samples
+                .iter()
+                .map(|v| v.to_vec())
+                .flatten()
+                .collect::<Vec<f32>>()
+                .as_slice(),
+            &[],
+        )?
+        .wait()?;
 
     let actual_outputs_buffer = gpu_tanh.propagate(&input_samples_buffer)?;
 
     let mut actual_outputs = vec![0.0; 100 * 100];
     let actual_outputs_slice = actual_outputs.as_mut_slice();
-    queue.enqueue_read_buffer(
-        &actual_outputs_buffer, 
-        CL_BLOCKING,
-        0, 
-        actual_outputs_slice, 
-        &[]
-    )?.wait()?;
+    queue
+        .enqueue_read_buffer(
+            &actual_outputs_buffer,
+            CL_BLOCKING,
+            0,
+            actual_outputs_slice,
+            &[],
+        )?
+        .wait()?;
 
-    assert_approx_equal_distance(&expected_outputs.iter().map(|v| v.to_vec()).flatten().collect(), &actual_outputs, 0.2);
+    assert_approx_equal_distance(
+        &expected_outputs
+            .iter()
+            .map(|v| v.to_vec())
+            .flatten()
+            .collect(),
+        &actual_outputs,
+        0.2,
+    );
+
+    Ok(())
+}
+
+#[test]
+fn should_return_same_value_on_back_propagation_as_normal_tanh_function() -> Result<(), ClError> {
+    let device_ids = get_all_devices(CL_DEVICE_TYPE_GPU)?;
+    let first_device = Device::new(device_ids[0]);
+
+    let context = Context::from_device(&first_device)?;
+    let queue = CommandQueue::create_with_properties(&context, device_ids[0], 0, 0)?;
+
+    let mut normal_tanh = TanH::new();
+    let mut gpu_tanh = TanHGPU::new(100, &context, &queue)?;
+
+    let input_samples = vec![vec![0.412; 100]; 100];
+    let first_derivatives = vec![vec![2.3; 100]; 100];
+
+    let mut input_samples_buffer =
+        Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, 100 * 100, ptr::null_mut())?;
+    let mut first_derivatives_buffer =
+        Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, 100 * 100, ptr::null_mut())?;
+
+    queue
+        .enqueue_write_buffer(
+            &mut first_derivatives_buffer,
+            CL_BLOCKING,
+            0,
+            first_derivatives
+                .iter()
+                .map(|v| v.to_vec())
+                .flatten()
+                .collect::<Vec<f32>>()
+                .as_slice(),
+            &[],
+        )?
+        .wait()?;
+
+    queue
+        .enqueue_write_buffer(
+            &mut input_samples_buffer,
+            CL_BLOCKING,
+            0,
+            input_samples
+                .iter()
+                .map(|v| v.to_vec())
+                .flatten()
+                .collect::<Vec<f32>>()
+                .as_slice(),
+            &[],
+        )?
+        .wait()?;
+
+    gpu_tanh.propagate(&input_samples_buffer)?;
+    normal_tanh.propagate(&input_samples);
+
+    let expected_loss_to_input_derivatives = normal_tanh
+        .back_propagate(true, &first_derivatives, 0.0)
+        .unwrap();
+
+    let actual_loss_to_input_derivatives_buffer = gpu_tanh
+        .back_propagate(true, &first_derivatives_buffer, 0.0)?
+        .unwrap();
+    let mut actual_loss_to_input_derivatives = vec![0.0; 100 * 100];
+    let actual_loss_to_input_derivatives_slice = actual_loss_to_input_derivatives.as_mut_slice();
+    queue
+        .enqueue_read_buffer(
+            &actual_loss_to_input_derivatives_buffer,
+            CL_NON_BLOCKING,
+            0,
+            actual_loss_to_input_derivatives_slice,
+            &[],
+        )?
+        .wait()?;
+
+    println!(
+        "derivatives CPU: {:?}",
+        &expected_loss_to_input_derivatives
+            .iter()
+            .map(|v| v.to_vec())
+            .flatten()
+            .collect::<Vec<f32>>()
+    );
+    println!("derivatives GPU: {:?}", &actual_loss_to_input_derivatives);
+
+    assert_approx_equal_distance(
+        &actual_loss_to_input_derivatives,
+        &expected_loss_to_input_derivatives
+            .iter()
+            .map(|v| v.to_vec())
+            .flatten()
+            .collect(),
+        0.2,
+    );
 
     Ok(())
 }
 
 #[derive(Debug, Savefile)]
 pub struct TanHGPU<'a> {
+    pub inputs_amount: usize,
+
     #[savefile_ignore]
     #[savefile_introspect_ignore]
     pub last_inputs_buffer: Option<&'a Buffer<cl_float>>,
@@ -97,35 +206,36 @@ pub struct TanHGPU<'a> {
     pub opencl_propagate_kernel: Option<Kernel>,
     #[savefile_ignore]
     #[savefile_introspect_ignore]
-    pub opencl_back_propagate_kernel: Option<Kernel>
+    pub opencl_back_propagate_kernel: Option<Kernel>,
 }
 
 impl<'a> TanHGPU<'a> {
     #[allow(dead_code)]
-    fn new(context: &'a Context, queue: &'a CommandQueue) -> Result<TanHGPU<'a>, ClError> {
+    fn new(inputs_amount: usize, context: &'a Context, queue: &'a CommandQueue) -> Result<TanHGPU<'a>, ClError> {
         let program_compilation_result =
             Program::create_and_build_from_source(context, PROGRAM_SOURCE, "");
-        if program_compilation_result .is_err() {
+        if program_compilation_result.is_err() {
             println!(
                 "A compilation error was found in the tanh.cl Program:\n{:?}",
-                program_compilation_result .err().unwrap()
+                program_compilation_result.err().unwrap()
             );
             println!("Please report this issue at https://github.com/gabrielmfern/intricate");
             panic!();
         }
 
-        let program = program_compilation_result .unwrap();
+        let program = program_compilation_result.unwrap();
         let propagation_kernel = Kernel::create(&program, PROPAGATE_KERNEL_NAME)?;
         let back_propagation_kernel = Kernel::create(&program, BACK_PROPAGATE_KERNEL_NAME)?;
 
         Ok(TanHGPU {
+            inputs_amount,
             opencl_context: Some(context),
             opencl_queue: Some(queue),
             opencl_program: Some(program),
             opencl_propagate_kernel: Some(propagation_kernel),
             opencl_back_propagate_kernel: Some(back_propagation_kernel),
             last_outputs_buffer: None,
-            last_inputs_buffer: None
+            last_inputs_buffer: None,
         })
     }
 }
@@ -138,10 +248,10 @@ impl<'a> OpenCLLayer<'a> for TanHGPU<'a> {
     ) -> Result<(), ClError> {
         let program_compilation_result =
             Program::create_and_build_from_source(context, PROGRAM_SOURCE, "");
-        if program_compilation_result .is_err() {
+        if program_compilation_result.is_err() {
             println!(
                 "A compilation error was found in the tanh.cl Program:\n{:?}",
-                program_compilation_result .err().unwrap()
+                program_compilation_result.err().unwrap()
             );
             println!("Please report this issue at https://github.com/gabrielmfern/intricate");
             panic!();
@@ -190,10 +300,7 @@ impl<'a> OpenCLLayer<'a> for TanHGPU<'a> {
         Ok(())
     }
 
-    fn propagate(
-        &mut self,
-        inputs: &'a Buffer<cl_float>,
-    ) -> Result<&Buffer<cl_float>, ClError> {
+    fn propagate(&mut self, inputs: &'a Buffer<cl_float>) -> Result<&Buffer<cl_float>, ClError> {
         assert!(self.opencl_context.is_some());
         assert!(self.opencl_queue.is_some());
         if self.last_inputs_buffer.is_some() {
@@ -202,7 +309,7 @@ impl<'a> OpenCLLayer<'a> for TanHGPU<'a> {
         if self.last_outputs_buffer.is_some() {
             drop(self.last_outputs_buffer.as_ref().unwrap());
         }
-        
+
         self.last_inputs_buffer = Some(inputs);
 
         let outputs_total_count = inputs.size()? / mem::size_of::<cl_float>();
@@ -211,7 +318,7 @@ impl<'a> OpenCLLayer<'a> for TanHGPU<'a> {
             self.opencl_context.unwrap(),
             CL_MEM_READ_WRITE,
             outputs_total_count,
-            ptr::null_mut()
+            ptr::null_mut(),
         )?;
 
         ExecuteKernel::new(self.opencl_propagate_kernel.as_ref().unwrap())
@@ -236,21 +343,24 @@ impl<'a> OpenCLLayer<'a> for TanHGPU<'a> {
             assert!(self.opencl_context.is_some());
             assert!(self.opencl_queue.is_some());
 
-            let outputs_total_count = 
-                self.last_outputs_buffer.as_ref().unwrap().size()? / mem::size_of::<cl_float>();
+            let samples_amount =
+                self.last_outputs_buffer.as_ref().unwrap().size()? / self.inputs_amount / mem::size_of::<cl_float>();
+
+            assert_eq!(samples_amount % 1, 0);
 
             let loss_to_input_derivatives_buffer = Buffer::<cl_float>::create(
                 self.opencl_context.unwrap(),
                 CL_MEM_READ_WRITE,
-                outputs_total_count,
-                ptr::null_mut()
+                self.inputs_amount * samples_amount,
+                ptr::null_mut(),
             )?;
 
             ExecuteKernel::new(self.opencl_back_propagate_kernel.as_ref().unwrap())
                 .set_arg(layer_output_to_error_derivative)
                 .set_arg(self.last_outputs_buffer.as_ref().unwrap())
                 .set_arg(&loss_to_input_derivatives_buffer)
-                .set_global_work_size(outputs_total_count)
+                .set_arg(&(self.inputs_amount as cl_int))
+                .set_global_work_sizes(&[samples_amount, self.inputs_amount])
                 .enqueue_nd_range(self.opencl_queue.unwrap())?
                 .wait()?;
 
