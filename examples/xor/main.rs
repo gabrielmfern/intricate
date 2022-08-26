853004a81612ac3edf33d2cf889c389752a4f292
Gabriel Miranda <88866334+gabrielmfern@users.noreply.github.com>
2022-08-26 09:03:19 -0300

    fix the device type in the XoR example

--- examples/xor/main.rs
+++ examples/xor/main.rs
@@ -36,7 +36,7 @@ fn main() -> () {
     // Actually instantiate the Model with the layers
     let mut xor_model = Model::new(layers);
     //            you can change this to DeviceType::GPU if you want
-    let opencl_state = setup_opencl(DeviceType::GPU).unwrap();
+    let opencl_state = setup_opencl(DeviceType::CPU).unwrap();
     xor_model.init(&opencl_state).unwrap();
 
     let mut loss = MeanSquared::new();
