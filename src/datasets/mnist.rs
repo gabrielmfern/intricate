a364783183a1c81ce1c7daa5e3fd63cffcf0124a
gabriel miranda <gabrielmfern@outlook.com>
2022-09-08 19:29:41 -0300

    fix a few bugs with the dataset reading and make the MNIST dataset use the generic functions

--- src/datasets/mnist.rs
+++ src/datasets/mnist.rs
@@ -1,3 +1,7 @@
+use rayon::prelude::*;
+
+use super::{read_3d_ubyte_file, read_1d_ubyte_file};
+
 const IMAGES_SOURCE: &[u8] = include_bytes!("mnist_data/mnist-train-images.idx3-ubyte");
 const LABELS_SOURCE: &[u8] = include_bytes!("mnist_data/mnist-train-labels.idx1-ubyte");
 
@@ -9,80 +13,44 @@ const TEST_IMAGES_LABELS: &[u8] = include_bytes!("mnist_data/mnist-test-labels.i
 ///
 /// Will also normalize the colors from `0 to 1` by dividing by **255*8.
 pub fn get_training_inputs() -> Vec<Vec<f32>> {
-    let mut sample_inputs: Vec<Vec<f32>> = Vec::with_capacity(60_000);
-
-    let image_size = 28 * 28;
-    for (i, byte) in IMAGES_SOURCE.iter().skip(16).enumerate() {
-        let sample_index = (i as f64 / image_size as f64).floor() as usize;
-
-        if sample_inputs.len() == sample_index {
-            if sample_index > 0 {
-                assert_eq!(sample_inputs[sample_index - 1].len(), image_size);
-            }
-
-            sample_inputs.push(Vec::with_capacity(image_size));
-        }
-
-        sample_inputs[sample_index].push(*byte as f32 / 255.0);
-    }
-
-    assert_eq!(sample_inputs.len(), 60_000);
-
-    sample_inputs
+    println!("reading the MNIST digit database images");
+    read_3d_ubyte_file(IMAGES_SOURCE, |byte| byte as f32 / 255.0)
+        .par_iter()
+        .map(|image| image.par_iter().flatten().map(|x| *x).collect::<Vec<f32>>())
+        .collect()
 }
 
 /// Gets the training labels of the MNIST dataset ready to be given as input to a Intricate model.
 pub fn get_training_outputs() -> Vec<Vec<f32>> {
-    let mut samples: Vec<Vec<f32>> = Vec::with_capacity(60_000);
-
-    for byte in LABELS_SOURCE.iter().skip(8) {
-        let mut output = vec![0.0; 10];
-        output[*byte as usize] = 1.0;
-
-        samples.push(output);
-    }
-
-    assert_eq!(samples.len(), 60_000);
-
-    samples
+    println!("reading the MNIST digit database labels");
+    read_1d_ubyte_file(LABELS_SOURCE)
+        .par_iter()
+        .map(|digit_index| {
+            let mut output = vec![0.0; 10];
+            output[*digit_index as usize] = 1.0;
+            output
+        })
+        .collect()
 }
 
 /// Gets the inputs for testing the Model that should be associated with their respective outputs
 pub fn get_test_inputs() -> Vec<Vec<f32>> {
-    let mut sample_inputs: Vec<Vec<f32>> = Vec::with_capacity(10_000);
-
-    let image_size = 28 * 28;
-    for (i, byte) in TEST_IMAGES_SOURCE.iter().skip(16).enumerate() {
-        let sample_index = (i as f64 / image_size as f64).floor() as usize;
-
-        if sample_inputs.len() == sample_index {
-            if sample_index > 0 {
-                assert_eq!(sample_inputs[sample_index - 1].len(), image_size);
-            }
-
-            sample_inputs.push(Vec::with_capacity(image_size));
-        }
-
-        sample_inputs[sample_index].push(*byte as f32 / 255.0);
-    }
-
-    assert_eq!(sample_inputs.len(), 10_000);
-
-    sample_inputs
+    println!("reading the MNIST digit testing database images");
+    read_3d_ubyte_file(TEST_IMAGES_SOURCE, |byte| byte as f32 / 255.0)
+        .par_iter()
+        .map(|image| image.par_iter().flatten().map(|x| *x).collect::<Vec<f32>>())
+        .collect()
 }
 
 /// Gets the outputs for testing the Model that should be associated with their respective inputs
 pub fn get_test_outputs() -> Vec<Vec<f32>> {
-    let mut samples: Vec<Vec<f32>> = Vec::with_capacity(10_000);
-
-    for byte in TEST_IMAGES_LABELS.iter().skip(8) {
-        let mut output = vec![0.0; 10];
-        output[*byte as usize] = 1.0;
-
-        samples.push(output);
-    }
-
-    assert_eq!(samples.len(), 10_000);
-
-    samples
+    println!("reading the MNIST digit testing database labels");
+    read_1d_ubyte_file(TEST_IMAGES_LABELS)
+        .par_iter()
+        .map(|digit_index| {
+            let mut output = vec![0.0; 10];
+            output[*digit_index as usize] = 1.0;
+            output
+        })
+        .collect()
 }
--- src/datasets/mod.rs
+++ src/datasets/mod.rs
@@ -3,20 +3,21 @@
 //! Currently contains the following datasets.
 //! - MNIST
 
-use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+use indicatif::ProgressIterator;
 
 /// The module containing the MNIST dataset
 #[cfg(feature = "mnist")]
 pub mod mnist;
 
+#[allow(dead_code)]
 fn get_dimensions_of_ubyte_dataset(source: &[u8], dimensions_amount: usize) -> Vec<usize> {
     let mut dimensions = Vec::with_capacity(dimensions_amount);
 
     for dimension in 0..dimensions_amount {
         let bytes = source
             .iter()
-            .skip(4 * dimension)
-            .take(4 * (dimension + 1))
+            .skip(4 * (dimension + 1))
+            .take(4 * (dimension + 2))
             .map(|b| *b)
             .collect::<Vec<u8>>();
 
@@ -26,14 +27,21 @@ fn get_dimensions_of_ubyte_dataset(source: &[u8], dimensions_amount: usize) -> V
     dimensions
 }
 
+#[allow(dead_code)]
 fn read_1d_ubyte_file(source: &[u8]) -> Vec<f32> {
     source
-        .par_iter()
+        .iter()
+        .skip(8) // skip the magic number
+        .progress()
         .map(|byte| *byte as f32)
         .collect::<Vec<f32>>()
 }
 
-fn read_3d_ubyte_file<TransformationFunc>(source: &[u8], transformation: TransformationFunc) -> Vec<Vec<Vec<f32>>>
+#[allow(dead_code)]
+fn read_3d_ubyte_file<TransformationFunc>(
+    source: &[u8],
+    transformation: TransformationFunc,
+) -> Vec<Vec<Vec<f32>>>
 where
     TransformationFunc: Fn(u8) -> f32,
 {
@@ -44,9 +52,9 @@ where
 
     let mut data: Vec<Vec<Vec<f32>>> = Vec::with_capacity(samples_amount);
 
-    for (i, byte) in source.iter().enumerate() {
+    for (i, byte) in source.iter().skip(16).enumerate().progress() {
         let sample_index = (i as f64 / (width * height) as f64).floor() as usize;
-        let row_index = i % width;
+        let row_index = ((i % (width * height)) as f64 / width as f64).floor() as usize;
 
         if data.len() == sample_index {
             if sample_index > 0 {
@@ -54,14 +62,14 @@ where
             }
 
             data.push(Vec::with_capacity(height));
-        } else {
-            if data[sample_index].len() == row_index {
-                if row_index > 0 {
-                    assert_eq!(data[sample_index][row_index - 1].len(), width);
-                }
+        }
 
-                data[sample_index].push(Vec::with_capacity(width));
+        if data[sample_index].len() == row_index {
+            if row_index > 0 {
+                assert_eq!(data[sample_index][row_index - 1].len(), width);
             }
+
+            data[sample_index].push(Vec::with_capacity(width));
         }
 
         data[sample_index][row_index].push(transformation(*byte));
